2c6fc8ca4f3de1c0a19c0f0d1a2d93f61e688ffd
Simen Sægrov <saegrov@gmail.com>
2013-05-20 03:14:32 +0200

    Added basic support for listing projects

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('readyNoteStackApp', [])
+angular.module('readyNoteStackApp', ['ngResource'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/projects.js
+++ app/scripts/controllers/projects.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('readyNoteStackApp')
-  .controller('ProjectsCtrl', function ($scope) {
+  .controller('ProjectsCtrl', function ($scope, $resource) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.Project = $resource('/projects/:projectId', {},
+      {'query': {method: 'GET', isArray: true}});
+
+
+    $scope.listProjects = $scope.Project.query();
+
   });
